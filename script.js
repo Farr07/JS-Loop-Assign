1f721044d7ad17ed11e38e0c5a7be51e3531225c
Farr07 <farr07ukh@gmail.com>
2024-02-29 04:09:17 -0800

    loop assigment completed

---
+++ script.js
@@ -0,0 +1,39 @@
+                //  (1) loop assigment
+
+
+// let table = +prompt('Enter Your Table')
+// let tables = +prompt('Enter Your table  start value')
+// let tablee = +prompt('Enter Your Table End Value')
+
+// for (let i = tables; i <=tablee; i++){
+//     document.write(table + 'x' + i +'=' + table*i + '<br>' )
+// 
+
+                  // (2) index
+
+
+// let arr =['Farrukh','Aaira','07','03'] ;                
+// let result = arr.indexOf('07')
+// document.write(result);
+
+                 //  (3) includes
+
+
+// let arr = ['farrukh','aaira'] ;            
+// let result = arr.includes('aaira')
+// document.write(result);
+
+                //(4) concat
+         
+//  let arr = ['farrukh','aaira'] ;  
+// let arr1 =['Farrukh','Aaira','07','03'] ;                
+// let result = arr.concat(arr1)
+// document.write(result);               
+
+
+                //(5) sort
+
+//    let arr = ['farrukh','aaira'] ;            
+// let result = arr.sort()
+// document.write(result);               
+
